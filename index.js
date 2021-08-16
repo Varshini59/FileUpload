03c3294ba6f63c0d237d73458604e06fc5b0cac1
Varshini59 <varsh52@gmail.com>
2021-08-16 15:00:46 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.redirect('/upload')
+});
+
+app.get("/upload", (req, res) => {
+  res.render("form.ejs");
+});
+
+const upload = multer({
+  dest: "uploads",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    // console.log(typeof file.originalname)
+    const fileName = file.originalname.split(".")[0];
+
+    if (!file.originalname.endsWith(".pdf")) {
+      return cb(new Error("Please upload a PDF"));
+    }
+    if (fileName.length !== 9) {
+        return cb(new Error('Invalid Filename!'))
+    }
+    return cb(undefined, true);
+  },
+});
+
+app.post("/",upload.single("upload"),(req, res) => {
+    res.send("Your file has been uploaded successfully!");
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+app.listen(3000, () => {
+  console.log("Server is running!");
+});
